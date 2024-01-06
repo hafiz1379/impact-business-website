fad874ab4817d26f1bdf7bb08210cd8a8c3d8df8
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-06 12:56:05 +0430

    Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       fontSize: {
-        logo: "30px",
-        navitem: "16px",
-        h1: "78px",
-        h2: "32px",
-        h3: "24px",
-        p: "16px",
+        logo: '30px',
+        navitem: '16px',
+        h1: '78px',
+        h2: '32px',
+        h3: '24px',
+        p: '16px',
       },
       backgroundColor: {
-        main: "#00796b",
+        main: '#00796b',
       },
       textColor: {
-        main: "#ffffff",
-        second: "#f85040",
+        main: '#ffffff',
+        second: '#f85040',
       },
       fontFamily: {
-        main: ["Montserrat", "sans-serif"],
-        secondary: ["Poppins", "sans-serif"],
-        default: ["Open Sans", "sans-serif"],
+        main: ['Montserrat', 'sans-serif'],
+        secondary: ['Poppins', 'sans-serif'],
+        default: ['Open Sans', 'sans-serif'],
       },
       fontWeight: {
         700: 700,
