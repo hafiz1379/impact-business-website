926a5d5e724574dd2a77a2f2990d8f0200d0aa0f
mhnazary <mhnazary2020@gmail.com>
2024-01-06 21:50:16 +0430

    Fix linter configuration error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,21 +23,21 @@ function Nav() {
   };
 
   return (
-    <nav className="z-10 relative flex flex-wrap justify-between items-center lg:px-16  bg-main text-[#ffffff99] font-700 h-20">
+    <nav className='z-10 relative flex flex-wrap justify-between items-center lg:px-16  bg-main text-[#ffffff99] font-700 h-20'>
       {/* Logo */}
-      <Link to="/" aria-label="Home" className={` ${isMenuOpen && 'hidden'}`}>
-        <h1 className="text-logo text-main font-main px-6">
+      <Link to='/' aria-label='Home' className={` ${isMenuOpen && 'hidden'}`}>
+        <h1 className='text-logo text-main font-main px-6'>
           Impact
-          <span className="text-second rounded-[50%]">.</span>
+          <span className='text-second rounded-[50%]'>.</span>
         </h1>
       </Link>
 
       {/* Toggle Menu Button */}
-      <div className="pr-3">
+      <div className='pr-3'>
         <button
-          label="Toggle Menu"
+          label='Toggle Menu'
           onClick={toggleMenu}
-          type="button"
+          type='button'
           className={`lg:hidden focus:ring-2 focus:ring-gray-200 p-2 rounded-lg ${
             isMenuOpen && 'hidden'
           }`}
@@ -47,9 +47,9 @@ function Nav() {
 
         {/* Close Menu Button */}
         <button
-          label="Close Menu"
+          label='Close Menu'
           onClick={toggleMenu}
-          type="button"
+          type='button'
           className={`lg:hidden focus:ring-2 focus:ring-gray-200 p-2 rounded-lg absolute top-0 right-0 mt-6 mr-3 ${
             !isMenuOpen && 'hidden'
           }`}
@@ -60,7 +60,7 @@ function Nav() {
 
       {/* Menu List */}
       <div className={`${!isMenuOpen && 'hidden'} w-full lg:w-auto lg:block`}>
-        <ul className="bg-main lg:space-x-10 lg:bg-transparent rounded-lg md:py-4 pt-14 flex flex-col lg:flex-row text-p md:font-700 w-full px-6">
+        <ul className='bg-main lg:space-x-10 lg:bg-transparent rounded-lg md:py-4 pt-14 flex flex-col lg:flex-row text-p md:font-700 w-full px-6'>
           {ROUTES.map(({ path, label }, i) => (
             <li
               className={`lg:hover:bg-transparent hover:text-second py-2  cursor-pointer rounded bg-main ${
