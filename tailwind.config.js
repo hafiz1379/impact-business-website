13be4b805232979becf4b177d91325b9e0908063
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-06 12:28:10 +0430

    Add tailwind.config.js with custom styles

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontSize: {
+        logo: "30px",
+        navitem: "16px",
+        h1: "78px",
+        h2: "32px",
+        h3: "24px",
+        p: "16px",
+      },
+      backgroundColor: {
+        main: "#00796b",
+      },
+      textColor: {
+        main: "#ffffff",
+        second: "#f85040",
+      },
+      fontFamily: {
+        main: ["Montserrat", "sans-serif"],
+        secondary: ["Poppins", "sans-serif"],
+        default: ["Open Sans", "sans-serif"],
+      },
+      fontWeight: {
+        700: 700,
+        600: 600,
+      },
+    },
+  },
+  plugins: [],
+};
