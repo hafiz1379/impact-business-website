37c6a5c9be50e0517b559131706fd217716f9162
mhnazary <mhnazary2020@gmail.com>
2024-01-07 23:47:35 +0430

    Some extra border color deleted

--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -3,7 +3,7 @@ import ServiceHeader from './ServiceHeader';
 export default function Service() {
   return (
     <>
-      <div className="border-2 border-emerald-400 bg-[#F6F6F6] text-center py-10">
+      <div className="bg-[#F6F6F6] text-center py-10">
         <ServiceHeader />
       </div>
     </>
