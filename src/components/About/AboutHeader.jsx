0e1f7bcda6502bccd42b4914b6664cc989aaa8e9
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-07 20:59:39 +0430

    Update AboutHeader styling

--- src/components/About/AboutHeader.jsx
+++ src/components/About/AboutHeader.jsx
@@ -1,6 +1,6 @@
 function AboutHeader() {
   return (
-    <div className="my-16 flex-center flex-col px-4 sm:px-16 md:mt-64">
+    <div className="my-16 flex-center flex-col px-4 sm:px-16 lg:mt-64">
       <h2 className="font-main text-h2">About Us</h2>
       <span className="-mt-3 mb-2 text-green2 text-h2">━━━</span>
       <p className="text-center text-pageHeader tracking-wide">
