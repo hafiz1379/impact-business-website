d74948f9db162cca95b11034293bc8c40cb49092
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-07 17:36:50 +0430

    Add About section to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
 import Nav from '../components/Navbar';
 import HomeTitle from '../components/Home/HomeTitle';
+import AboutHeader from '../components/About/AboutHeader';
+import AboutBody from '../components/About/AboutBody';
+import ClientLogos from '../components/About/ClientLogos';
+import AboutStatistics from '../components/About/AboutStatistics';
 
 function Home() {
   return (
     <>
       <Nav />
       <HomeTitle />
+      <AboutHeader />
+      <AboutBody />
+      <ClientLogos />
+      <AboutStatistics />
     </>
   );
 }
