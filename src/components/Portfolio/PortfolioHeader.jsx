54462ca30a6a5c9f65c84193036b664179fae61a
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-08 16:50:25 +0430

    Add PortfolioHeader component

---
+++ src/components/Portfolio/PortfolioHeader.jsx
@@ -0,0 +1,12 @@
+import Header from '../Header';
+
+function PortfolioHeader() {
+  return (
+    <Header
+      title="Portfolio"
+      desc="Quam sed id excepturi ccusantium dolorem ut quis dolores nisi llum nostrum enim velit qui ut et autem uia reprehenderit sunt deleniti"
+    />
+  );
+}
+
+export default PortfolioHeader;
