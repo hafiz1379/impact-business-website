7f1710fdf0e047b516997f2e6d38ba9527c2605f
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-08 16:50:53 +0430

    Refactor Home.jsx to import Portfolio and About components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,17 @@
 import Nav from '../components/Navbar';
 import HomeTitle from '../components/Home/HomeTitle';
-import AboutHeader from '../components/About/AboutHeader';
-import AboutBody from '../components/About/AboutBody';
-import ClientLogos from '../components/About/ClientLogos';
-import AboutStatistics from '../components/About/AboutStatistics';
 import Service from '../components/Service/Service';
+import Portfolio from '../components/Portfolio/Portfolio';
+import About from '../components/About/About';
 
 function Home() {
   return (
     <>
       <Nav />
       <HomeTitle />
-      <AboutHeader />
-      <AboutBody />
-      <ClientLogos />
-      <AboutStatistics />
+      <About />
       <Service />
+      <Portfolio />
     </>
   );
 }
