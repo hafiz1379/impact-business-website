d1da471a1a3a4684ab33f0fcc188d6dfd99b8d3f
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-08 16:50:16 +0430

    Add About component

---
+++ src/components/About/About.jsx
@@ -0,0 +1,17 @@
+import AboutBody from './AboutBody';
+import AboutHeader from './AboutHeader';
+import AboutStatistics from './AboutStatistics';
+import ClientLogos from './ClientLogos';
+
+function About() {
+  return (
+    <div className="lg:mt-60">
+      <AboutHeader />
+      <AboutBody />
+      <ClientLogos />
+      <AboutStatistics />
+    </div>
+  );
+}
+
+export default About;
