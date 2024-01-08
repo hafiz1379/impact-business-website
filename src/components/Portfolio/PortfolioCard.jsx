3eb5dafea21b23b38f1a4f93a0c5e17bb2216cd7
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-08 23:08:33 +0430

    Update PortfolioCard styles

--- src/components/Portfolio/PortfolioCard.jsx
+++ src/components/Portfolio/PortfolioCard.jsx
@@ -1,12 +1,12 @@
 /*eslint-disable*/
 function PortfolopCard({ img, title, desc }) {
     return (
-        <div className="w-full shadow-md rounded-lg ">
+        <div className="w-full shadow-md rounded-lg bg-[#fff]">
           <div className="group overflow-hidden  rounded-t-lg">
             <img src={img} alt="" className="w-full transition-transform transform scale-100 group-hover:scale-110 duration-300 hover:cursor-pointer" />
           </div>
           <div className="p-4">
-            <h3 className="text-h3 font-bold">{title}</h3>
+            <h3 className="text-xl font-semibold">{title}</h3>
             <p className='text-p'>{desc}</p>
           </div>
         </div>
