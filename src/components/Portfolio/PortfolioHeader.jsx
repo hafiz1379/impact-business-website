2940987d168a64a32181fa699dd9c60c9a5e9ec9
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-08 23:08:22 +0430

    Refactor PortfolioHeader component

--- src/components/Portfolio/PortfolioHeader.jsx
+++ src/components/Portfolio/PortfolioHeader.jsx
@@ -2,10 +2,12 @@ import Header from '../Header';
 
 function PortfolioHeader() {
   return (
-    <Header
-      title="Portfolio"
-      desc="Quam sed id excepturi ccusantium dolorem ut quis dolores nisi llum nostrum enim velit qui ut et autem uia reprehenderit sunt deleniti"
-    />
+    <div className="-mt-24">
+      <Header
+        title="Portfolio"
+        desc="Quam sed id excepturi ccusantium dolorem ut quis dolores nisi llum nostrum enim velit qui ut et autem uia reprehenderit sunt deleniti"
+      />
+    </div>
   );
 }
 
