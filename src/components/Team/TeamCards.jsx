7fcfff370cfbb38b3be130ffac9c7821f65be750
mhnazary <mhnazary2020@gmail.com>
2024-01-10 00:16:09 +0430

    Added teamcard component

--- src/components/Team/TeamCards.jsx
+++ src/components/Team/TeamCards.jsx
@@ -1,17 +1,41 @@
 /*eslint-disable*/
+import TeamCard from "./TeamCard";
+
+const info = [
+  {
+    name: "Walter White",
+    position: "Web development",
+    Image: "/images/team/team-1.jpg",
+  },
+  {
+    name: "Walter Green",
+    position: "Marketing",
+    Image: "/images/team/team-2.jpg",
+  },
+  {
+    name: "Walter Black",
+    position: "Accounting",
+    Image: "/images/team/team-3.jpg",
+  },
+  {
+    name: "Walter Purple",
+    position: "Full-stack developer",
+    Image: "/images/team/team-4.jpg",
+  },
+];
 
 function TeamCards() {
   return (
     <>
-      <div className="border-2 border-red-500 xl:px-5 w-full mt-10 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        <div className="border-2 border-y-yellow-400 w-full p-4 xl:mx-2">
-          <img
-            className="w-full rounded-lg"
-            src="/images/Team/team-1.jpg"
-            alt="alina"
+      <div className="w-full px-8 mt-10 grid md:grid-cols-2 lg:grid-cols-3 xl:px-16 xl:grid-cols-4 gap-6">
+        {info.map((item, index) => (
+          <TeamCard
+            key={index}
+            name={item.name}
+            position={item.position}
+            image={item.Image}
           />
-          <h1 className="">Walter white</h1>
-        </div>
+        ))}
       </div>
     </>
   );
