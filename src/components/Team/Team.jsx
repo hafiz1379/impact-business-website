a03c922bc9fb4f0e8139a7029762932c718c2e7c
mhnazary <mhnazary2020@gmail.com>
2024-01-09 22:39:45 +0430

    Added team Photo

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -1,4 +1,5 @@
 import TeamHeader from '../Service/ServiceHeader';
+import TeamCards from './TeamCards';
 
 function Team() {
   const cardDes = 'Aperiam dolorum et et wuia molestias qui eveniet numquam nihil porro incidunt dolores placeat sunt id nobis omnis tiledo stran delop';
@@ -6,6 +7,7 @@ function Team() {
     <>
       <div className="border-2 border-blue-500 py-10">
         <TeamHeader title="Our Team" description={cardDes} />
+        <TeamCards />
       </div>
     </>
   );
---
+++ src/components/Team/TeamCards.jsx
@@ -0,0 +1,20 @@
+/*eslint-disable*/
+
+function TeamCards() {
+  return (
+    <>
+      <div className="border-2 border-red-500 xl:px-5 w-full mt-10 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <div className="border-2 border-y-yellow-400 w-full p-4 xl:mx-2">
+          <img
+            className="w-full rounded-lg"
+            src="/images/Team/team-1.jpg"
+            alt="alina"
+          />
+          <h1 className="">Walter white</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default TeamCards;
