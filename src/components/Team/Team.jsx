769de9bae42374de26d8e033e8fa16b891ce189a
mhnazary <mhnazary2020@gmail.com>
2024-01-09 20:57:07 +0430

    Add teamheader component

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -1,7 +1,12 @@
+import TeamHeader from '../Service/ServiceHeader';
+
 function Team() {
+  const cardDes = 'Aperiam dolorum et et wuia molestias qui eveniet numquam nihil porro incidunt dolores placeat sunt id nobis omnis tiledo stran delop';
   return (
     <>
-      <h1>Team part</h1>
+      <div className="border-2 border-blue-500 py-10">
+        <TeamHeader title="Our Team" description={cardDes} />
+      </div>
     </>
   );
 }
