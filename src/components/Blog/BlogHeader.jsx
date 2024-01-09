b852edf279e8c8ddc8e71320a1d1aac8b1bd47cf
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-09 10:00:58 +0430

    Add BlogHeader component

---
+++ src/components/Blog/BlogHeader.jsx
@@ -0,0 +1,14 @@
+function BlogHeader() {
+  return (
+    <div className="bg-main text-main text-center py-14 px-4 md:px-12 lg:px-16">
+      <h2 className="text-[56px] font-secondary">Blog</h2>
+      <p>
+        Odio et unde deleniti. Deserunt numquam exercitationem. Officiis quo
+        odio sint voluptas consequatur ut a odio voluptatem. Sit dolorum debitis
+        veritatis natus dolores. Quasi ratione sint. Sit quaerat ipsum dolorem.
+      </p>
+    </div>
+  );
+}
+
+export default BlogHeader;
