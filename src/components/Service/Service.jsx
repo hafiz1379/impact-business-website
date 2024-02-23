650cda06ff0893fe2f6cf3698c5109fd6303e58f
hafiz1379 <hafizrasa1379@gmail.com>
2024-02-23 09:15:04 +0430

    Refactor Service component imports and update card description

--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-
-import ServiceHeader from './ServiceHeader';
-import ServiceCard from './ServiceCard';
-import ServiceSlider from './ServiceSlider';
+/* eslint-disable */
+import React from "react";
+import ServiceHeader from "./ServiceHeader";
+import ServiceCard from "./ServiceCard";
+import ServiceSlider from "./ServiceSlider";
 
 export default function Service() {
-  const cardDes = 'Aperiam dolorum et et wuia molestias qui eveniet numquam nihil porro incidunt dolores placeat sunt id nobis omnis tiledo stran delop';
+  const cardDes =
+    "Aperiam dolorum et et wuia molestias qui eveniet numquam nihil porro incidunt dolores placeat sunt id nobis omnis tiledo stran delop";
   return (
     <>
-      <div className="bg-[#F6F6F6] text-center py-10">
+      <div className="bg-[#F6F6F6] text-center py-10" id="services">
         <ServiceHeader title="Our Services" description={cardDes} />
         <div className="cards w-full mt-10 flex flex-wrap justify-center px-2 lg:px-0">
           <ServiceCard />
