37eb3d16237e1a5a630b1c79a1ba9a26a4d31423
hafiz1379 <hafizrasa1379@gmail.com>
2024-02-23 09:19:10 +0430

    Refactor Contact component to add id to parent div

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-
 import ContactForm from './ContactForm';
 import ContactHeader from './ContactHeader';
 
 function Contact() {
   return (
-    <div className="pb-16">
+    <div className="pb-16" id="contact">
       <ContactHeader />
       <ContactForm />
     </div>
