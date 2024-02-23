6cd332febc6b6527f898e4da3cb46ab11f827dec
hafiz1379 <hafizrasa1379@gmail.com>
2024-02-23 09:30:08 +0430

    Add Node.js environment to .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "node": true // Add Node.js environment
     },
     "extends": [
         "eslint:recommended",
@@ -11,7 +12,7 @@ module.exports = {
         "ecmaFeatures": {
             "jsx": true
         },
-        "ecmaVersion": 2021, // Updated ecmaVersion
+        "ecmaVersion": 2021,
         "sourceType": "module"
     },
     "plugins": [
